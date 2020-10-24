f0db6f8edc1754ad2458ecf1fd39f8f42f3bdf54
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-10-24 21:04:34 +0100

    basics-assignment-2-start

---
+++ .vscode practice/basics-assignment-2-start/basics-assignment-2-start/assignment.js
@@ -0,0 +1,31 @@
+const task3Element = document.getElementById('task-3');
+
+function firstName() {
+  alert('just trying')
+}
+
+function surname(name) {
+alert('I am' + name);
+}
+
+function combine(try1, try2, try3) {
+  const combinedText = `${try1} ${try2} ${try3}`;
+}
+return combinedText;
+
+surname( ' Ehiawey')
+
+task3Element.addEventListener('click' greet);
+
+const combinedString = combine('It is fine', 'trying', 'out!' );
+alert(combinedString);
+function combine(try1, try2, try3) {
+  const combinedText = `${try1} ${try2} ${try3}`;
+}
+return combinedText;
+
+surname( ' Ehiawey')
+task3Element.addEventListener('click', greet);
+
+const combinedString = combine('It is fine', 'trying', 'out!' );
+alert(combinedString);
