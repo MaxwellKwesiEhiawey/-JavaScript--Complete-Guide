c763f775c3840d743ce07a0cf488382cccd0dd98
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-10-24 09:48:36 +0100

    Update app.js

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert('This Works!');
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = "(" + currentResult + " + 10 ) * 3 / 2 - 1";
+
+outputResult(currentResult, calculationDescription);
