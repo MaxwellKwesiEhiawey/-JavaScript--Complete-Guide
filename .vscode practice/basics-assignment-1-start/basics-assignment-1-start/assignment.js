952f2ce957620be69d3f24b00362f438db90fca9
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-10-24 17:04:48 +0100

    .vscode practice

---
+++ .vscode practice/basics-assignment-1-start/basics-assignment-1-start/assignment.js
@@ -0,0 +1,15 @@
+let userInput = 6;
+
+let inputResult;
+
+inputResult =  18 + userInput;
+
+inputResult = inputResult - 3;
+
+inputResult = inputResult * 4
+
+inputResult = inputResult/2
+
+alert(userInput)
+
+alert(inputResult)
