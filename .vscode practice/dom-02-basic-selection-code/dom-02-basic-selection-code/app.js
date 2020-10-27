a9319e097095579b5588f693f3430bf1bb57ba4b
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-10-27 07:22:07 +0000

    Update app.js

--- .vscode practice/dom-02-basic-selection-code/dom-02-basic-selection-code/app.js
+++ .vscode practice/dom-02-basic-selection-code/dom-02-basic-selection-code/app.js
@@ -2,7 +2,7 @@ const h1 = document.getElementById('main-title');
 
 h1.textContent = 'Some new title!';
 h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+h1.style.backgroundColor = 'blue';
 
 const li = document.querySelector('li:last-of-type');
 li.textContent = li.textContent + ' (Changed!)';
