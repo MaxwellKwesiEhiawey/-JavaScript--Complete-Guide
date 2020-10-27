b1e7d9155227daafbc25e7cdca84235f394e9535
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2020-10-27 20:06:43 +0000

    arrays-assignment-problem

---
+++ .vscode practice/arrays-assignment-problem/arrays-assignment-problem/assignment.js
@@ -0,0 +1,14 @@
+const createdArray = [2, 3, 4, 5, 6];
+
+const isBiggerThan5 = createdArray.filter(number => number > 5);
+console.log(isBiggerThan5);
+
+const createdArray2 = [ 8, 10]
+reversed = createdArray.reverse();
+console.log('reversed:', reversed);
+removeItem = createdArray.shift();
+concatedArray = createdArray.concat(createdArray2)
+console.log(removeItem);
+console.log(createdArray);
+console.log(concatedArray);
+
